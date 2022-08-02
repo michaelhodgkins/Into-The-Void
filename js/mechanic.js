8294610e22941d13d319061f5a02f49f231db438
Michael Hodgkins <michaelhodgkins@Michaels-MacBook-Pro.local>
2022-08-02 16:53:28 +0100

    end of day

--- js/mechanic.js
+++ js/mechanic.js
@@ -4,15 +4,6 @@ class CardGame {
     }
 
 
-shuffleCards() {
-    let copyCards = [...this.cards];
-    let shuffled = [];
-    for(let i = 0; i < this.cards.length; i++) {
-        let randomIndex = Math.floor(Math.random() * copyCards.length);
-        shuffled.push(copyCards[randomIndex]);
-        copyCards.splice(randomIndex, 1);
-        }
-    this.cards = shuffled;
-    }
+
 
 }
