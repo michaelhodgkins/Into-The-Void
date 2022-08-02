505237fc5a574f7ab2e8ade57191c8e648d19239
Michael Hodgkins <michaelhodgkins@Michaels-MacBook-Pro.local>
2022-08-02 14:45:45 +0100

    game overhaul, cards now randomise

--- js/index.js
+++ js/index.js
@@ -4,11 +4,17 @@ let gameOverScreen = document.querySelector("#gameOver");
 let gameScreen = document.querySelector("#game");
 let splashDiv = document.querySelector(".start-div");
 let gameScreenContainer = document.querySelector(".game-board");
+
+let playerCardElement = document.querySelector('#player');
+let computerCardElement = document.querySelector('#computer');
+let buttonElement = document.querySelector('#play');
+
 let player1 = 0;
 let player2 = 0;
 
+let discardCards = [];
 //cards
-const cards = [
+const playerCards = [
   { name: 'blue', img: '../images/card1.png', value: 1},
   { name: 'cat', img: '../images/card2.jpeg', value: 1},
   { name: 'wolf', img: '../images/card3.jpeg', value: 1 },
@@ -26,59 +32,63 @@ const cards = [
   { name: 'robin', img: '../images/card15.jpeg', value: 3 },
 ];
 
-const cardGame = new CardGame(cards);
-let playerCard = cards[Math.floor(Math.random()* cards.length)];
-let computerCard = cards[Math.floor(Math.random()* cards.length)];
+const computerCards = [
+  { name: 'blue', img: '../images/card1.png', value: 1},
+  { name: 'cat', img: '../images/card2.jpeg', value: 1},
+  { name: 'wolf', img: '../images/card3.jpeg', value: 1 },
+  { name: 'dragon', img: '../images/card4.jpeg', value: 1 },
+  { name: 'brain', img: '../images/card5.jpeg', value: 1 },
+  { name: 'lizard', img: '../images/card6.jpeg', value: 2 },
+  { name: 'void', img: '../images/card7.jpeg', value: 2 },
+  { name: 'skull', img: '../images/card8.jpeg', value: 2 },
+  { name: 'warrior', img: '../images/card9.jpeg', value: 2 },
+  { name: 'tree', img: '../images/card10.jpeg', value: 2 },
+  { name: 'clouds', img: '../images/card11.jpeg', value: 3 },
+  { name: 'robot', img: '../images/card12.jpeg', value: 3 },
+  { name: 'joker', img: '../images/card13.jpeg', value: 3 },
+  { name: 'batman', img: '../images/card14.jpeg', value: 3 },
+  { name: 'robin', img: '../images/card15.jpeg', value: 3 },
+];
+
+const cardGame = new CardGame(playerCards);
+let playerCard
+let computerCard
+function getRandomCards() {
+let playerRandomIndex = Math.floor(Math.random()* playerCards.length); 
+playerCard = playerCards[playerRandomIndex];
+discardCards.push(playerCard);
+playerCards.splice(playerRandomIndex, 1);
+let computerRandomIndex = Math.floor(Math.random()* computerCards.length);
+computerCard = computerCards[computerRandomIndex];
+discardCards.push(computerCard);
+computerCards.splice(playerRandomIndex, 1);
+playerCardElement.setAttribute("style", `background-image: url(${playerCard.img})`);
+computerCardElement.setAttribute("style", `background-image: url(${computerCard.img})`);
+}
+
+function compareCards(card1, card2) {
+  if(card1.value > card2.value){
+    player1++;
+  } else if(card1.value < card2.value){
+    player2++;
+  }
+}
+
+
+// for (let i = 0; i < 15; i++) {
+//   getRandomCards();
+//   compareCards(playerCard,computerCard);
+//   console.log(playerCard,computerCard);
+// }
+
+console.log(player1, player2);
 
 let html = "";
 window.addEventListener("load", (event) => {
   gameOverScreen.style.display = "none";
   gameScreen.style.display = "none";
   gameScreenContainer.style.display = "none";
-  cardGame.shuffleCards(cards);
-
-
-    
-     html += `
-       <div class="card" data-card-name="${playerCard.name}">
-         <div class="back" name="${playerCard.img}"></div>
-         <div class="front" style="background: url(img/${playerCard.img}) no-repeat"></div>
-       </div>
-       `;
-       console.log(playerCard);
-  
-       
-  
-    console.log(computerCard);
-    
-     html += `
-       <div class="card" data-card-name="${computerCard.name}">
-         <div class="back" name="${computerCard.img}"></div>
-         <div class="front" style="background: url(img/${computerCard.img}) no-repeat"></div>
-       </div>
-       `;
-       console.log(computerCard);
-
-console.log(html);
-document.querySelector("#game").innerHTML = html;
 }); 
-
-const gameBegin = (playerCard, computerCard) => {
-  if(playerCard.value >  computerCard.value) {
-    player1++;
-  }
-  if(playerCard.value < computerCard.value){
-    player2++
-  }
-
-  }
-gameBegin(playerCard,computerCard);
-console.log(player1);
-console.log(player2);
-
-
-
-
 
     startBtn.addEventListener("click", () => {
       splashScreen.style.display = "none";
@@ -86,4 +96,9 @@ console.log(player2);
       gameScreen.style.display = "flex";
       gameScreenContainer.style.display = "block";
       splashDiv.style.display = "none";
+    });
+
+    buttonElement.addEventListener("click", () => {
+      getRandomCards()
+      
     });
